e3e56fdfc9d36eafa37e38a9f566cbafeb73817e
Rifaa-Haque <159312173+Rifaa-Haque@users.noreply.github.com>
2024-03-06 06:27:14 -0500

    Inject the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -60,7 +60,36 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">
+      ${day}
+    </div>
+    <div class="weather-forecast-icon">⛅</div>
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max">
+        <em>18°</em>
+      </span>
+      <span class="weather-forecast-temperature-min">
+        <em>12°</em>
+      </span>
+    </div>
+  </div>
+  `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form-id");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("New York");
+displayForecast();
